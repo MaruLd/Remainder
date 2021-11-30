b125ab47490ceffa5a8a1842e5bd85e1ad54c383
Maru <accnumberthree@gmail.com>
2021-11-30 18:17:31 +0700

    Initial commit

---
+++ HealthReminder/MainForm.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace HealthReminder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numSecond = new System.Windows.Forms.NumericUpDown();
+            this.numMinute = new System.Windows.Forms.NumericUpDown();
+            this.numHour = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStudy = new System.Windows.Forms.Button();
+            this.btnRelax = new System.Windows.Forms.Button();
+            this.btnExe = new System.Windows.Forms.Button();
+            this.btnSaveMsg = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSongChoose = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(8, 173);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(137, 66);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 329);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 61);
+            this.textBox1.TabIndex = 1;
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("Sylfaen", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TimeLabel.Location = new System.Drawing.Point(55, 35);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(195, 62);
+            this.TimeLabel.TabIndex = 3;
+            this.TimeLabel.Text = "00:00:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(81, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Minute";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(155, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Second";
+            // 
+            // numSecond
+            // 
+            this.numSecond.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numSecond.Location = new System.Drawing.Point(154, 131);
+            this.numSecond.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numSecond.Name = "numSecond";
+            this.numSecond.Size = new System.Drawing.Size(64, 27);
+            this.numSecond.TabIndex = 5;
+            // 
+            // numMinute
+            // 
+            this.numMinute.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numMinute.Location = new System.Drawing.Point(81, 132);
+            this.numMinute.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numMinute.Name = "numMinute";
+            this.numMinute.Size = new System.Drawing.Size(64, 27);
+            this.numMinute.TabIndex = 7;
+            // 
+            // numHour
+            // 
+            this.numHour.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numHour.Location = new System.Drawing.Point(8, 132);
+            this.numHour.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numHour.Name = "numHour";
+            this.numHour.Size = new System.Drawing.Size(64, 27);
+            this.numHour.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(8, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hour";
+            // 
+            // btnStudy
+            // 
+            this.btnStudy.Location = new System.Drawing.Point(227, 211);
+            this.btnStudy.Name = "btnStudy";
+            this.btnStudy.Size = new System.Drawing.Size(64, 28);
+            this.btnStudy.TabIndex = 10;
+            this.btnStudy.Text = "Study";
+            this.btnStudy.UseVisualStyleBackColor = true;
+            this.btnStudy.Click += new System.EventHandler(this.btnStudy_Click);
+            // 
+            // btnRelax
+            // 
+            this.btnRelax.Location = new System.Drawing.Point(227, 173);
+            this.btnRelax.Name = "btnRelax";
+            this.btnRelax.Size = new System.Drawing.Size(64, 28);
+            this.btnRelax.TabIndex = 11;
+            this.btnRelax.Text = "Relax";
+            this.btnRelax.UseVisualStyleBackColor = true;
+            this.btnRelax.Click += new System.EventHandler(this.btnRelax_Click);
+            // 
+            // btnExe
+            // 
+            this.btnExe.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExe.Location = new System.Drawing.Point(227, 248);
+            this.btnExe.Name = "btnExe";
+            this.btnExe.Size = new System.Drawing.Size(64, 28);
+            this.btnExe.TabIndex = 12;
+            this.btnExe.Text = "Excercise";
+            this.btnExe.UseVisualStyleBackColor = true;
+            this.btnExe.Click += new System.EventHandler(this.btnExe_Click);
+            // 
+            // btnSaveMsg
+            // 
+            this.btnSaveMsg.Location = new System.Drawing.Point(228, 362);
+            this.btnSaveMsg.Name = "btnSaveMsg";
+            this.btnSaveMsg.Size = new System.Drawing.Size(66, 28);
+            this.btnSaveMsg.TabIndex = 13;
+            this.btnSaveMsg.Text = "Save";
+            this.btnSaveMsg.UseVisualStyleBackColor = true;
+            this.btnSaveMsg.Click += new System.EventHandler(this.btnSaveMsg_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(8, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Message:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(228, 329);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(66, 28);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(227, 130);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(64, 28);
+            this.btnSet.TabIndex = 18;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStop.Font = new System.Drawing.Font("UD Digi Kyokasho NK-B", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(154, 173);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(64, 66);
+            this.btnStop.TabIndex = 15;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(8, 251);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 23);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // btnSongChoose
+            // 
+            this.btnSongChoose.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSongChoose.Location = new System.Drawing.Point(155, 248);
+            this.btnSongChoose.Name = "btnSongChoose";
+            this.btnSongChoose.Size = new System.Drawing.Size(63, 28);
+            this.btnSongChoose.TabIndex = 20;
+            this.btnSongChoose.Text = "Choose";
+            this.btnSongChoose.UseVisualStyleBackColor = true;
+            this.btnSongChoose.Click += new System.EventHandler(this.btnSongChoose_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Song";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(306, 402);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSongChoose);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSaveMsg);
+            this.Controls.Add(this.btnExe);
+            this.Controls.Add(this.btnRelax);
+            this.Controls.Add(this.btnStudy);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numHour);
+            this.Controls.Add(this.numMinute);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numSecond);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Reminder";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numSecond;
+        private System.Windows.Forms.NumericUpDown numMinute;
+        private System.Windows.Forms.NumericUpDown numHour;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStudy;
+        private System.Windows.Forms.Button btnRelax;
+        private System.Windows.Forms.Button btnExe;
+        private System.Windows.Forms.Button btnSaveMsg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSongChoose;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ HealthReminder/MainForm.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace HealthReminder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        System.Timers.Timer t;
+
+        int totalSecond;
+        TimeSpan timeleft;
+        public String Msg = "Time up!";
+
+        String songPath = "";
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            loadPlayList();
+            songPath = @"playlist\Nếu.wav";
+            t = new System.Timers.Timer();
+            t.Interval = 1000;
+            t.Elapsed += OnTimeEvent;
+            
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Trace.WriteLine("total second: " + totalSecond);
+            if (totalSecond > 0)
+            {
+                totalSecond--;
+                timeleft = TimeSpan.FromSeconds(totalSecond);
+                Trace.WriteLine("total second: " + totalSecond);
+                timeLabel(timeleft.Hours, timeleft.Minutes, timeleft.Seconds);
+            }
+            else
+            {
+                timeLabel(0, 0, 0);
+                StartButtonChange();
+                t.Stop();
+                playSong();
+                MessageBox.Show(Msg);
+            }
+            }
+
+        private void loadPlayList()
+        {
+            String path = Directory.GetCurrentDirectory() + @"\playlist";
+            DirectoryInfo d = new DirectoryInfo(path);
+            FileInfo[] Files = d.GetFiles("*.wav");
+            List<Song> songs = new List<Song>();
+
+            int num = 0;
+            foreach (FileInfo file in Files)
+            {
+                songs.Add(new Song { name = file.Name, path = file.FullName });
+                //Trace.WriteLine(file.FullName);
+            }
+            var bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = songs;
+
+            comboBox1.DataSource = bindingSource1.DataSource;
+
+            comboBox1.DisplayMember = "name";
+            comboBox1.ValueMember = "path";
+        }
+
+        private void playSong()
+        {
+            SystemSounds.Asterisk.Play();
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(songPath);
+            player.Play();
+        }
+
+        private void pauseSong()
+        {
+            SystemSounds.Asterisk.Play();
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(songPath);
+            player.Play();
+        }
+
+
+        private void timeLabel(int h,int m,int s)
+        {
+            TimeLabel.Invoke((MethodInvoker)(() => TimeLabel.Text = string.Format("{0:D2}:{1:D2}:{2:D2}", h,m,s)));
+        }
+        
+        private void StartButtonChange()
+        {
+            btnStart.Invoke((MethodInvoker)(() => btnStart.BackColor = Color.ForestGreen));
+            btnStart.Invoke((MethodInvoker)(() => btnStart.Text = "Start"));
+            btnStart.Invoke((MethodInvoker)(() => btnStart.Click -= btnPause_Click));
+            btnStart.Invoke((MethodInvoker)(() => btnStart.Click += new EventHandler(btnStart_Click)));
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            btnStart.BackColor = Color.ForestGreen;
+            btnStart.Text = "Start";
+            btnStart.Click -= btnPause_Click;
+            btnStart.Click += new EventHandler(btnStart_Click);
+            t.Stop();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            totalSecond = 0;
+        }
+
+        private void btnRelax_Click(object sender, EventArgs e)
+        {
+            setTime(0, 15, 0);
+        }
+
+        private void btnStudy_Click(object sender, EventArgs e)
+        {
+            setTime(1, 0, 0);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            t.Start();
+            btnStart.BackColor = Color.YellowGreen;
+            btnStart.Text = "Pause";
+            btnStart.Click -= btnStart_Click;
+            btnStart.Click += new EventHandler(btnPause_Click);
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            int h = 0, m = 0, s = 0;
+            h = (int)numHour.Value;
+            m = (int)numMinute.Value;
+            s = (int)numSecond.Value;
+            setTime(h, m, s);
+            TimeLabel.Text = string.Format("{0:D2}:{1:D2}:{2:D2}", h, m, s);
+        }
+
+        private void btnExe_Click(object sender, EventArgs e)
+        {
+            setTime(0, 30, 0);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Msg = "Time up!";
+            textBox1.Text = "";
+        }
+
+        public void setTime(int h, int m, int s)
+        {
+            totalSecond = h * 3600 + m * 60 + s;
+            Trace.WriteLine("total second: " + totalSecond);
+        }
+
+
+        private void btnSaveMsg_Click(object sender, EventArgs e)
+        {
+            Msg = textBox1.Text;
+        }
+
+        private void btnSongChoose_Click(object sender, EventArgs e)
+        {
+            songPath = comboBox1.SelectedValue.ToString();
+        }
+    }
+}
